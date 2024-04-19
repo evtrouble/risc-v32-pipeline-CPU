d466ff7efbdb4911c48f5db55eea1855da63f1ec
evtrouble <105802875+1494938977@users.noreply.github.com>
2024-04-19 20:48:24 +0800

    Update fibonacci.asm

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,9 +1,8 @@
 #################################################
-#	x1		stack pointer
+#	x2		stack pointer
 #	x5		return addr
-#	x6		n = SW[14:10]
-#	x7		fib(n)
-#	x31		0xFFFF0000
+#	x6		31
+#	x7		fib(31)
 #################################################
 #	fib(n)
 #		n		fib			n		fib
@@ -14,40 +13,39 @@
 #		4		0x5			20		0x2ac2
 #		5		0x8			21		0x452f
 #		6		0xd			22		0x6ff1
-#		7		0x15		23		0xb520
-#		8		0x22		24		0x12511
-#		9		0x37		25		0x1da31
-#		10		0x59		26		0x2ff42
-#		11		0x90		27		0x4d973
-#		12		0xe9		28		0x7d8b5
-#		13		0x179		29		0xcb228
-#		14		0x262		30		0x148add
-#		15		0x3db		31		0x213d05
+#		7		0x15			23		0xb520
+#		8		0x22			24		0x12511
+#		9		0x37			25		0x1da31
+#		10		0x59			26		0x2ff42
+#		11		0x90			27		0x4d973
+#		12		0xe9			28		0x7d8b5
+#		13		0x179			29		0xcb228
+#		14		0x262			30		0x148add
+#		15		0x3db			31		0x213d05
 #################################################
 
 addi		x4,x0,0
 addi		x20,x0,0x7C
-sb			x20,1(x4)
+sb		x20,1(x4)
 
 main:
-	lw		x6, 0(x4)
+	lw	x6, 0(x4)
 	srli	x6, x6, 10
 	andi	x6, x6, 0x01F
-    addi	x7,x7,1
-    addi	x8,x8,-1
-    addi	x9,x0,0
-	jal		x5, fib
-	sw		x7, 4(x4)
-	#jal		x0, main
-    jal		x0,return
+    	addi	x7,x7,1
+   	addi	x8,x8,-1
+   	addi	x9,x0,0
+	jal	x5, fib
+	sw	x7, 4(x4)
+   	jal	x0,return
 
 fib:
 	addi	x8,x8,1
-	bge		x8, x6, ret
-    add		x10,x7,x0
-    add		x7,x7,x9
-    add		x9,x10,x0
-	jal		fib
+	bge	x8, x6, ret
+    	add	x10,x7,x0
+    	add	x7,x7,x9
+   	add	x9,x10,x0
+	jal	fib
 ret:
 	jalr	x0, x5, 0
 return:
